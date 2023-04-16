d70357ad5bf21c355278bf841f121f983021dd07
Daniel Rhodes <danielrhodes47@gmail.com>
2023-04-16 14:14:23 -0500

    first commit

---
+++ starter_code/script.js
@@ -0,0 +1,37 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+	let inputVal = input.value.toLowerCase();
+	const lowerStr = str.map(str => str.toLowerCase())
+	lowerStr.filter((val) => {
+	  if (inputVal.includes(inputVal)) {
+		results.push(val);
+	  }
+	});
+	return results;
+  }
+
+
+function searchHandler(e) {
+	const results = search(fruit);
+	const inputVal = e.target.value;
+	showSuggestions(results, inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+
+	
+}
+
+function useSuggestion(e) {
+	// TODO
+}
+
+
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
